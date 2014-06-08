c218b3203ddaf138936739fba76c5d6c450007b3
Kelly <jkellysenna@gmail.com>
2014-06-07 21:50:21 -0400

    Merge branch 'master' of https://github.com/ksenna/vynl
    Conflicts:
    	Gemfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,37 @@ class UsersController < ApplicationController
 	before_filter :authenticate_user!
 
 	def index
+<<<<<<< HEAD
 		@user = current_user
+=======
+		# @user = current_user
+		# response = Echowrap.song_search(artist: "Weezer")
+		# response = Echowrap.song_search(:artist)
+		# @artist = response[0].artist_name
+		@user = current_user
+		@new_profile = Profile.new
+<<<<<<< HEAD
+=======
+	end
+
+	def create
+		@new_profile = Profile.new(profile_params)
+		if @profile.save
+			redirect_to users_path, notice: "Item added."
+		else
+			redirect_to :back, alert: "Failed to save."
+		end
+>>>>>>> FETCH_HEAD
+	end
+
+	def create
+		@new_profile = Profile.new(profile_params)
+		if @profile.save
+			redirect_to users_path, notice: "Item added."
+		else
+			redirect_to :back, alert: "Failed to save."
+		end
+>>>>>>> origin/login
 	end
 
 	def home
